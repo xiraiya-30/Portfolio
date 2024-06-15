cac122e652999824c7b8e219d963d3c5153120b7
xiraya <srivarsen30@gmail.com>
2024-06-16 01:50:40 +0530

    final

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,7 @@ export default function Projects () {
     return (
         <div className="flex flex-col justify-center items-center align-middle w-screen h-screen">
             <h1>Projects</h1>
-            <p>This is the projects page 🗿 .</p>
+            <p>This is the projects page 🗿 gggfbng .</p>
             <h1 className=" text-4xl text-black">Comming soon.</h1>
         </div>
     )
