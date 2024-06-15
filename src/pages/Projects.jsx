d1409e1a28b5f5275599e8b2ddc453f6dd2a3b12
xiraya <srivarsen30@gmail.com>
2024-06-16 00:26:32 +0530

    retard style workflow chekking push

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Projects {
+    return (
+        <div className="flex items-center w-screen h-screen">
+            <h1>Projects</h1>
+            <p>This is the projects page.</p>
+        </div>
+    )
+}
