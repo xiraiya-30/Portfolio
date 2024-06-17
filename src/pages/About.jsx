9d0b1978a7b27d2e7dc306c06714d7caebd711d7
xiraya <srivarsen30@gmail.com>
2024-06-17 21:16:32 +0530

    modified about data 🗿

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,13 +14,13 @@ export default function About() {
             <div className="flex flex-col md:flex-row justify-center sm:align-middle h-screen ">
 
             <div className="w-vw flex animate-pop-down justify-center items-center order-1 md:order-2 mt-5 md:mt-0">
-                <img className="w-44 md:w-[400px]" src={avatar} alt="Profile Photo" />
+                <img className="w-44 md:w-[400px] " src={avatar} alt="Profile Photo" />
             </div>
             <div className="w-full animate-pop-left md:w-1/2  flex flex-col font-sans font-black py-5 justify-center items-center md:items-start order-2 md:order-1">
                 <h1 className="text-black text-2xl md:text-4xl ">Hi, I'm Srivarsen,</h1>
                 <br />
                 <p className="text-gray-200 opacity-80 text-[25px] sm:text-xl md:text-2.2xl text-center font-sans md:text-left tracking-wider">
-                    A passionate AI&DS student blending the power of full stack development with advanced AI and machine learning to create innovative solutions for tomorrow's challenges.
+                    On my way to defy the limits of today's tech. Because why settle for the ordinary when you can code the future? Wubba lubba dub dub! 
                 </p>
                 <br />
                 <div className="flex justify-center md:justify-start space-x-6 w-full">
