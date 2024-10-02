51ae71a6a52c04c482edfcd8fb2641830967d460
xiraya <srivarsen30@gmail.com>
2024-10-02 09:02:28 +0530

    deploy test 2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs.send(service_id, template_id, details)
+    emailjs.send(service_id, template_id, details,public_id)
       .then(() => {
         setMsgSent(true);
         setLoading(false);
